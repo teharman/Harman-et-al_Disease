6d9a21e45ca3772dafdf957a777c5da7dc530cd6
Tyler E. Harman <50343995+teharman@users.noreply.github.com>
2022-09-14 15:43:38 -0400

    Update Melanin_Code.R

--- Melanin_Code.R
+++ Melanin_Code.R
@@ -1,76 +1,95 @@
-setwd("D:/Tyler Thesis/R Data Files/Immunity Data")
-
+setwd("D:/Other/Tyler Thesis/R Data Files/Immunity Data")
 Melanin_Data<-read.csv("Melanin_Data.csv",header=T)
-source("http://goo.gl/UUyEzD") #outlier code source
-outlierKD(Melanin_Data, Melanin_mg) #outlier code
 
 library(ggpubr)
 library(rstatix)
 library(ggplot2)
 library(tidyverse)
 library(naniar)
+library(lme4)
+library(sjPlot)
+library(sjmisc)
+library(glmmTMB)
+library(ggplot2)
+library(tidyverse)
+library(reshape)
+library(ggpubr)
+library(Rmisc)
+library(lsmeans)
+library(ggthemes)
+library(stats)
 
-Melanin_Data$Melanin_mg<-log(Melanin_Data$Melanin_mg)
+source("http://goo.gl/UUyEzD") #outlier code source
+outlierKD(Melanin_Data, Melanin_mg) #outlier code
+
+#determination of normality
 model<-lm(Melanin_mg~Collection*Treatment*Sym.State,data=Melanin_Data)
 ggqqplot(residuals(model))
 shapiro.test(residuals(model))
-Melanin_Data %>% levene_test(Melanin_mg~Collection*Treatment*Sym.State)
-Melanin.av<-aov(Melanin_mg~Treatment*Sym.State*Collection, data = Melanin_Data)
-summary(Melanin.av)
-TukeyHSD(Melanin.av)
+
+data.lm<-glm(Melanin_mg~Treatment*Sym.State*Collection,data = Melanin_Data,family=Gamma(link="inverse"))
+summary(data.lm)
+tiff("Melanin_Plot.tiff", height = 18, width = 18, units = 'cm', 
+     compression = "lzw", res = 300)
+par(mfrow=c(2,2))
+plot(data.lm)
+dev.off()
+
+PWC1<-lsmeans(data.lm,specs=c("Treatment","Sym.State"))
+summary(PWC1)
+rbind(pairs(PWC1),adjust="none")
+
+PWC2<-lsmeans(data.lm,specs=c("Collection","Sym.State"))
+summary(PWC2)
+rbind(pairs(PWC2),adjust="none")
+
+PWC3<-lsmeans(data.lm,specs=c("Treatment","Collection"))
+summary(PWC3)
+rbind(pairs(PWC3),adjust="none")
+
+PWC4<-lsmeans(data.lm,specs=c("Treatment","Sym.State","Collection"))
+summary(PWC4)
+rbind(pairs(PWC4),adjust="none")
 
 ####Treatment:Sym State####
 
-Melanin_Data$Treatment_Com<- with(Melanin_Data, paste0(Sym.State, Treatment)) #merging column items together
+label1 <- "A[1]"
+label2 <- "A[2]"
+label3 <- "B[1]"
+label4 <- "B[2]"
 
-Boxplot1<-ggplot(Melanin_Data,mapping=aes(x=Treatment_Com,y=Melanin_mg,color=Treatment,linetype=Treatment))+
+Melanin_Data$Treatment_Com<- with(Melanin_Data, paste0(Sym.State, Treatment, Collection)) #merging column items together
+
+tiff("Melanin_Data.tiff", height = 12, width = 24, units = 'cm', 
+     compression = "lzw", res = 300)
+
+ggplot(Melanin_Data,mapping=aes(x=Treatment_Com,y=Melanin_mg,colour=Collection,linetype=Treatment))+
   geom_boxplot(alpha=0.3,fatten=2,lwd=1, outlier.size = 3)+
   labs(x="Symbiotic State",y="Melanin (mg/cm^2)")+
   ylab(bquote('Melanin (mg '~cm^-2*')'))+
   theme(plot.title = element_text(vjust=2))+
-  theme_bw()+
+  theme_stata()+
   ylim(0,0.05)+
+  facet_grid(. ~ Sym.State,scales="free")+
+  theme(strip.text.x = element_text(size=15))+
+  scale_color_manual(values = c("grey60","grey40"))+
   theme(plot.title = element_text(color="black", size=20, face="bold.italic", vjust=4, hjust=0.5),
-        axis.title.x = element_text(size=14,vjust=-2),
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12),
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.text.y = element_text(size=12,hjust=0.5),
         axis.title.y = element_text(size=14,vjust=6),
-        plot.margin=unit(c(1,1,1,2),"cm"))+
-  theme(legend.text = element_text(colour="black", size=15),
+        plot.margin=unit(c(0.5,0.5,0.5,1),"cm"))+
+  theme(axis.ticks.x = element_blank())+
+  theme(legend.text = element_text(colour="black", size=12),
         legend.key.size = unit(1.5, "cm"),
         legend.title=element_blank(),
         legend.position = "right")+
-  scale_x_discrete(labels=c("AposymbioticAmbient" = "Aposymbiotic 18°C", "AposymbioticElevated" = "Aposymbiotic 26°C","SymbioticAmbient" = "Symbiotic 18°C","SymbioticElevated"="Symbiotic 26°C"))+
-  annotate(geom="text",x=1,y=0.05,label="A",size=10)+
-  annotate(geom="text",x=3,y=0.05,label="B",size=10)+
-  annotate(geom="text",x=2,y=0.05,label="AB",size=10)+
-  annotate(geom="text",x=4,y=0.05,label="AB",size=10)
-Boxplot1
-
-####Sym State:Collection####
-
-Melanin_Data$Sym_State_Com<- with(Melanin_Data, paste0(Sym.State, Collection))
-
-Boxplot2<-ggplot(Melanin_Data,mapping=aes(x=Sym_State_Com,y=Melanin_mg,color=Sym.State,linetype=Sym.State))+
-  geom_boxplot(alpha=0.3,fatten=2,lwd=1, outlier.size = 3)+
-  labs(x="Symbiotic State",y="Melanin (mg/cm^2)")+
-  ylab(bquote('Melanin (mg '~cm^-2*')'))+
-  theme(plot.title = element_text(vjust=2))+
-  theme_bw()+
-  ylim(0,0.05)+
-  theme(plot.title = element_text(color="black", size=20, face="bold.italic", vjust=4, hjust=0.5),
-        axis.title.x = element_text(size=14,vjust=-2),
-        axis.text.x = element_text(size=12),
-        axis.text.y = element_text(size=12),
-        axis.title.y = element_text(size=14,vjust=6),
-        plot.margin=unit(c(1,1,1,2),"cm"))+
-  theme(legend.text = element_text(colour="black", size=15),
-        legend.key.size = unit(1.5, "cm"),
-        legend.title=element_blank(),
-        legend.position = "right")+
-  scale_x_discrete(labels=c("AposymbioticWinter" = "Aposymbiotic Winter", "AposymbioticSummer" = "Aposymbiotic Summer","SymbioticWinter" = "Symbiotic Winter","SymbioticSummer"="Symbiotic Summer"))+
-  annotate(geom="text",x=1,y=0.05,label="AB",size=10)+
-  annotate(geom="text",x=3,y=0.05,label="AB",size=10)+
-  annotate(geom="text",x=2,y=0.05,label="A",size=10)+
-  annotate(geom="text",x=4,y=0.05,label="B",size=10)
-Boxplot2
+  annotate(geom = "text", x = 1, y = 0.025, label = deparse(bquote(~A[1])),
+           color = "grey55", size = 8,parse=T)+
+  annotate(geom = "text", x = 1, y = 0.035, label = deparse(bquote(~A[2])),
+           color = "grey55", size = 8,parse=T)+
+  annotate(geom = "text", x = 3, y = 0.045, label = deparse(bquote(~B[1])),
+           color = "grey55", size = 8,parse=T)+
+  annotate(geom = "text", x = 4, y = 0.045, label = deparse(bquote(~B[2])),
+           color = "grey55", size = 8,parse=T)
+dev.off()
